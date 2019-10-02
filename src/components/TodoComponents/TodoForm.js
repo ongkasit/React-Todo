107fc27e7d9fb7f9c140d29fe523ecabd349bce0
Kasit Nganthavee <kasit.nganthavee@gmail.com>
2019-10-03 06:48:35 +0700

    built ToDo Form

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" />
+                <button>Add</button>
+            </form>
+        );
+    }
+}
+
+export default TodoForm;
